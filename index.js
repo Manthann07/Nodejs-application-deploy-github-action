309558f066ae0f44346867c01b8d7f1bebbabf8b
Manthann07 <manthanbhadaliya123@gmail.com>
2026-05-04 14:30:49 +0530

    Update home route message

--- index.js
+++ index.js
@@ -4,8 +4,10 @@ const app = express();
 const PORT = process.env.PORT ?? 8080
 
 app.get('/', (req, res) => {
-    return res.json({ message: 'Hello World! from server' });
-})
+    return res.json({
+        message: 'Node.js application successfully deployed on AWS EC2 using GitHub Actions!'
+    });
+});
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
